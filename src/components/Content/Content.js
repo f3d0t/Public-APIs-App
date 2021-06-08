34ff64a757ccafcacb69ea359ad8b40008f08b43
f3d0t <logrus7@gmail.com>
2021-06-09 00:22:33 +0300

    fix bugs

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,7 +8,7 @@ import styles from './Content.css';
 export function Content({ error, isDataLoading, apiArray, filterArrays, filters, displayRandom }) {
   if (error !== null)
     return <p class={styles.loading_text + ' ' + styles.loading_text__error}>{error}</p>;
-  else if (isDataLoading) return <p class={styles.loading_text}>Data is loading</p>;
+  if (isDataLoading) return <p class={styles.loading_text}>Data is loading</p>;
 
   return (
     <Apis
