6380e844ef7987ecf92a3d12ce5a6cd5e91c86c2
f3d0t <logrus7@gmail.com>
2021-06-08 23:49:19 +0300

    delete unnecessary functions

--- src/data/filterData.js
+++ src/data/filterData.js
@@ -1,35 +1,3 @@
-import { checkBooleanAndConvert } from './utils';
-import { renderApp } from '../framework/render';
-
-export function setFilter(key, value, activeInputId) {
-  if (value === 'All' || value === '') {
-    window.dataStore.filters[key] = '';
-  } else {
-    window.dataStore.filters[key] = checkBooleanAndConvert(value);
-  }
-  window.dataStore.displayRandom = false;
-  window.dataStore.activeInputId = activeInputId;
-  renderApp();
-}
-
-export function setRandom() {
-  window.dataStore.displayRandom = true;
-  renderApp();
-}
-
-export function clearFilters(render = true) {
-  let { filters } = window.dataStore;
-  Object.keys(filters).map(key => {
-    window.dataStore.filters[key] = '';
-  });
-  window.dataStore.displayRandom = false;
-  if (render) renderApp();
-}
-
-export function reloadApp() {
-  clearFilters(false);
-}
-
 export function filterApiArray(apiArray, filters, random) {
   Object.entries(filters).map(([key, value]) => {
     if (value !== '') {
